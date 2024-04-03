0db23d40b9a66c3f21959bec76ab67c4ebfb627d
Idigo Luwum <3513564+lvwvm@users.noreply.github.com>
2024-04-03 15:34:12 +0000

    fix: use edu.paschalcs package namespace.

---
+++ hello-world/src/main/edu/paschalcs/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.paschalcs;
+
+/**
+ * Example main src.
+ *
+ * @author 
+ */
+class HelloWorld {
+
+ public static void main(String[] args){
+  System.out.println("Hello World!");
+ }
+}
